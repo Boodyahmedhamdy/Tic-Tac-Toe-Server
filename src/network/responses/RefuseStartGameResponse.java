7dfec41d1c342eeec7b53889451a3fbae5424337
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-01-24 19:39:05 +0200

    updated to match exactly what is on the client side

--- src/network/responses/RefuseStartGameResponse.java
+++ src/network/responses/RefuseStartGameResponse.java
@@ -1,13 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package network.responses;
 
 /**
  *
- * @author HP
+ * @author Abdelrahman_Elshreif
  */
-public class RefuseStartGameResponse extends StartGameResponse{
+public class RefuseStartGameResponse extends StartGameResponse {
     
-    public RefuseStartGameResponse(String username) {
-        super(username);
+    public RefuseStartGameResponse(String senderUsername, String recieverUsername) {
+        super(senderUsername, recieverUsername);
     }
     
 }
