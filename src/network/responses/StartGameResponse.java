8f427023fc48abc869aa4926f8ab009b3889b18e
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-01-24 19:37:45 +0200

    updated to match exactly StartGameResponse  on the client side

--- src/network/responses/StartGameResponse.java
+++ src/network/responses/StartGameResponse.java
@@ -1,26 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package network.responses;
 
 /**
  *
- * @author HP
+ * @author Abdelrahman_Elshreif
  */
-public class StartGameResponse extends Response{
-    
-    protected String username;
 
-    public StartGameResponse(String username) {
-     
-        this.username = username;
+public class StartGameResponse extends Response {
+    private String senderUsername;
+    private String recieverUsername;
+
+    public String getSenderUsername() {
+        return senderUsername;
     }
 
-    public String getUsername() {
-        return username;
+    public void setSenderUsername(String senderUsername) {
+        this.senderUsername = senderUsername;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public String getRecieverUsername() {
+        return recieverUsername;
     }
-    
-    
-    
+
+    public void setRecieverUsername(String recieverUsername) {
+        this.recieverUsername = recieverUsername;
+    }
+
+    public StartGameResponse(String senderUsername, String recieverUsername) {
+        this.senderUsername = senderUsername;
+        this.recieverUsername = recieverUsername;
+    }
 }
