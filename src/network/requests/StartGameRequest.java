53d37b88958ccb56c9301a3bbcfe6d522a9aa8ab
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-01-24 19:38:33 +0200

    updated to match StartGameRequest  from the client side

--- src/network/requests/StartGameRequest.java
+++ src/network/requests/StartGameRequest.java
@@ -3,13 +3,29 @@ package network.requests;
 import java.io.Serializable;
 
 public class StartGameRequest extends Request implements Serializable {
-    private String username;
 
-    public StartGameRequest(String username) {
-        this.username = username;
+    private String senderUsername;
+    private String recieverUsername;
+
+    public StartGameRequest(String senderUsername, String recieverUsername) {
+        this.senderUsername = senderUsername;
+        this.recieverUsername = recieverUsername;
+    }
+    
+    public String getSenderUsername() {
+        return senderUsername;
     }
 
-    public String getUsername() {
-        return username;
+    public void setSenderUsername(String senderUsername) {
+        this.senderUsername = senderUsername;
     }
+
+    public String getRecieverUsername() {
+        return recieverUsername;
+    }
+
+    public void setRecieverUsername(String recieverUsername) {
+        this.recieverUsername = recieverUsername;
+    }
+
 }
